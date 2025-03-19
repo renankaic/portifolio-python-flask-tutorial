531df6d948ca5d872e018b56a4b9de6a4bf70c84
Renan Kaic Lopes <renan-kaic@outlook.com>
2025-03-19 13:04:04 -0300

    Feat: Initialize database and add command for database setup

--- flaskr/db.py
+++ flaskr/db.py
@@ -2,7 +2,7 @@ import sqlite3
 from datetime import datetime
 
 import click
-from flask import current_app, g
+from flask import Flask, current_app, g
 
 def get_db():
     if 'db' not in g:
@@ -24,13 +24,22 @@ def init_db():
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf-8'))
-    
+
+# The code below links the 'init-db' command with the 'flask' command line, allowing to execute 'flask --app flaskr init-db'
 @click.command('init-db')
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
 
+# tells Python how to interpret timestamp values in the database. It converts the value to a datetime.datetime
 sqlite3.register_converter(
     'timestamp', lambda v: datetime.fromisoformat(v.decode())
 )
+
+def init_app(app: Flask):
+    # tells Flask to call 'close_db' function when cleaning up after returning a response
+    app.teardown_appcontext(close_db)
+
+    # adds a new command that can be called with the 'flask' command
+    app.cli.add_command(init_db_command)
