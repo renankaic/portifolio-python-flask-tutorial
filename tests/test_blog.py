0f51e9bc930c9f35a707fe59306122e906605d35
Renan Kaic Lopes <renan-kaic@outlook.com>
2025-03-24 23:14:59 -0300

    Feat: Add unit tests for login requirements and author permissions in blog posts

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,4 +1,5 @@
 import pytest
+from flask import Flask
 from flaskr.db import get_db
 
 
@@ -17,3 +18,31 @@ def test_index(client, auth):
   assert b'by test on 2018-01-01' in response.data
   assert b'test\nbody' in response.data
   assert b'href="/1/update"' in response.data
+
+
+# pytest.mark.parametrize tells Pytest to run the same test function with different arguments
+# You use it here to test different inputs
+@pytest.mark.parametrize('path', (
+  '/create',
+  '/1/update',
+  '/1/delete'
+))
+def test_login_required(client, path):
+  response = client.post(path)
+  assert response.header['Location'] == '/auth/login'
+
+
+def test_author_required(app: Flask, client, auth):
+  # change the post author to another user
+  with app.app_context():
+    db = get_db()
+    db.execute('UPDATE post SET author_id = 2 WHERE id = 1')
+    db.commit()
+
+  auth.login()
+  # current user can't modify other user's post
+  assert client.post('/1/update').status_code == 403
+  assert client.post('/1/delete').status_code == 403
+  # current user doesn't see edit link
+  assert b'href="/1/update"' not in client.get('/').data
+
