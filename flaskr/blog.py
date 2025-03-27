3cecf9b4ec9e0e289baa514a36ef4eeb4fd89177
Renan Kaic Lopes <renan-kaic@outlook.com>
2025-03-26 21:51:27 -0300

    Feat: Add tagging functionality for blog posts with tag management in the database

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -4,9 +4,12 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+import re
+
 bp = Blueprint("blog", __name__)
 
-def get_post(id, check_author=True):
+
+def get_post(id, check_author=True, bring_tags=True):
     post = get_db().execute(
         "SELECT p.id, title, body, created, author_id, username,(SELECT count(author_id) FROM post_likes WHERE post_id = p.id AND author_id = ?) as 'liked' ,(SELECT count(author_id) FROM post_likes WHERE post_id = p.id) as 'likes'"
         ' FROM post p JOIN user u ON p.author_id = u.id'
@@ -19,6 +22,10 @@ def get_post(id, check_author=True):
 
     if check_author and post['author_id'] != g.user['id']:
         abort(403)
+
+    if bring_tags:
+      post = dict(post)
+      post['tags'] = get_post_tags(id)
     
     return post
 
@@ -34,6 +41,43 @@ def get_post_comments(id):
 
     return post_comments
 
+
+def tags_handle_incoming(tags: str) -> list[str]:
+    tags = re.sub(r"[^a-zA-Z0-9\-,]", "", tags.replace(" ",""))
+    if "," in tags:
+      tags = tags.split(",")
+      for tag in tags:
+        if tag == "" or tag == "-":
+          tags.remove(tag)
+    else:
+       return [tags]
+    return tags
+
+
+def post_tags_insert_or_update(db, post_id: int, tags: list[str]):
+  for tag in tags:
+    db.execute(
+        'INSERT OR IGNORE INTO tags (tag_name) VALUES (?)',
+        (tag,)
+    )
+    db.execute(
+       'DELETE FROM post_tags WHERE post_id = ?',
+       (post_id,)
+    )
+    db.execute(
+        'INSERT OR IGNORE INTO post_tags (post_id, tag_name) VALUES (?,?)',
+        (post_id, tag)
+    )
+   
+
+def get_post_tags(id) -> list[str]:
+    tags = get_db().execute(
+        "SELECT tag_name FROM post_tags WHERE post_id = ?",
+        (id,)
+    ).fetchall()    
+    return [row['tag_name'] for row in tags]
+
+
 @bp.route("/")
 def index():
     db = get_db()
@@ -42,6 +86,10 @@ def index():
         " FROM post p JOIN user u ON p.author_id = u.id"
         " ORDER BY created DESC"
     ).fetchall()
+        
+    posts = [
+      {**dict(post), 'tags': get_post_tags(post["id"])} for post in posts
+    ]    
     return render_template("blog/index.html", posts=posts)
 
 
@@ -51,22 +99,34 @@ def create():
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
+        tags = request.form["tags"]
         error = None
 
         if not title:
             error = "Title is required."
+        
+        if not tags:
+          error = "Enter at least one tag."
+        else:
+          tags = tags_handle_incoming(tags)
+          if len(tags) == 0:
+            error = "Enter at least one tag."
           
         if error is not None:
             flash(error)
         else:
             db = get_db()
-            db.execute(
+            cur =  db.execute(
                 'INSERT INTO post (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
+            post_id = cur.lastrowid
+
+            post_tags_insert_or_update(db, post_id, tags)
+
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('blog.details', id=post_id))
         
     return render_template('blog/create.html')
 
@@ -87,10 +147,18 @@ def update(id):
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        tags = request.form['tags']
         error = None
 
         if not title:
-            error = 'Title is required'
+          error = 'Title is required'
+
+        if not tags:
+          error = "Enter at least one tag."
+        else:
+          tags = tags_handle_incoming(tags)
+          if len(tags) == 0:
+            error = "Enter at least one tag."
 
         if error is not None:
             flash(error)
@@ -101,6 +169,7 @@ def update(id):
                 ' WHERE id = ?',
                 (title, body, id)
             )
+            post_tags_insert_or_update(db, id, tags)
             db.commit()
             return redirect(url_for('blog.index'))
     
